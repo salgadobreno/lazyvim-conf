1b2ff8bef9069fa9c287c84b6ac2bc5f50fc17f4
Breno Salgado <salgado.breno@gmail.com>
2025-02-04 20:45:20 -0300

    using zbirenbaum/copilot

--- lua/plugins/buzaga.lua
+++ lua/plugins/buzaga.lua
@@ -1,4 +1,13 @@
 return {
   { "folke/noice.nvim", enabled = false },
-  { "github/copilot.vim", enabled = true },
+  { "github/copilot.vim", enabled = false },
+  {
+    "zbirenbaum/copilot.lua",
+    enabled = true,
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
 }
